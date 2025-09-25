76d53043e25a0f37cc9e51f7539c396eba40a2e0
Ryan Barthélémy <rbarthelemy.pro@gmail.com>
2025-09-25 20:42:59 +0200

    Mise à jours de l'exo 8

--- src/test/java/fr/ryan/Exo8.java
+++ src/test/java/fr/ryan/Exo8.java
@@ -8,15 +8,21 @@ public class Exo8 {
         Scanner clavier = new Scanner(System.in);
 
         // récupérer un phrase
-        System.out.print("Entrer un nombre (Entre 1 et 12) ");
-        int nombre = clavier.nextInt();
+        System.out.print("Entrer un mois (Entre 1 et 12) ");
+        int mois = clavier.nextInt();
 
         clavier.close();
-        
-        
 
-        
-        
+        if ( mois < 1 || mois > 12) {
+            System.out.println("Le mois doit être entre 1 et 12");
+        } else if ( mois == 1 || mois == 3 || mois == 5 || mois == 7 || mois == 8 || mois == 10 || mois == 12) {
+            System.out.println(31 + " Jours");
+        } else if ( mois == 4 || mois == 6 || mois == 9 || mois == 11 ) {
+            System.out.println(30 + " Jours");
+        } else {
+            System.out.println(28 + " Jours");
+        }
+
     }
 
 }
